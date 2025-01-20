14e7c44cec67d690f24fd7082e1429ec58086a7d
NamelessHerb <1327118667@qq.com>
2025-01-20 21:18:03 +0800

    Yes

--- main.py
+++ main.py
@@ -6,22 +6,54 @@ Given a string text, return true if it is a palindrome, or false otherwise.
 For this question, letters are NOT case-sensitive, for example, "LEVeL" is a palindrome.
 """
 # Change this function so it works correctly
-def is_palindrome(text):
-    return False
 
+def run_test():
+  test_cases = [('level', True),
+              ('LEVeL', True),
+              ('levy', False),
+              ('rotor', True),
+              ('motor', False),
+              ('No lemon no melon', True),
+              ('No No', False),
+              ('2025202', True),
+              ('100P', False),
+              ('Do geese see God', True)]
+  for test_text, expected in test_cases:
+      result = is_palindrome(test_text)
+      msg = 'Correct' if expected == result else 'Wrong'
+      print(f'[{msg:7s}] is_palindrome("{test_text}") should be {expected}')
 
-if __name__ == '__main__':
-    test_cases = [('level', True),
-                  ('LEVeL', True),
-                  ('levy', False),
-                  ('rotor', True),
-                  ('motor', False),
-                  ('No lemon no melon', True),
-                  ('No No', False),
-                  ('2025202', True),
-                  ('100P', False),
-                  ('Do geese see God', True)]
-    for test_text, expected in test_cases:
-        result = is_palindrome(test_text)
-        msg = 'Correct' if expected == result else 'Wrong'
-        print(f'[{msg:7s}] is_palindrome("{test_text}") should be {expected}')
+def is_palindrome(message):
+  message = message.replace(' ', '')
+  message = message.upper()
+  Keynum = len(message)
+
+  if Keynum % 2 == 0:
+      Key = int(Keynum / 2)
+  else:
+      Key = int((Keynum - 1) / 2)
+
+  needlist = []
+
+  for i in range(Key):
+      needlist.append(message[i])
+
+  yeslist = needlist[::-1]
+
+  if Keynum % 2 == 0:
+      Round = Key
+  else:
+      Round = Key + 1
+
+  Check = []
+
+  for i in range(Round, Keynum):
+      Check.append(message[i])
+
+  if Check == yeslist:
+      return True
+
+  else:
+      return False
+
+run_test()
